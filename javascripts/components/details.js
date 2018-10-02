8b6fd17ac0e070bf77c1484ff3767dc0514628d7
Your NameAftan <aftan.merrida@gmail.com>
2018-10-02 18:53:21 -0500

    firstcommit

---
+++ javascripts/components/details.js
@@ -0,0 +1,7 @@
+const detailsBuilder = (character) => {
+    console.log('character inside details', character);
+//this has to have a import because we are passing a object
+
+};
+
+export{detailsBuilder}; 
